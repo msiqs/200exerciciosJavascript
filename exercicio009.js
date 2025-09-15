f6e24c3c8ad1a9fedb985a8fcb6e5013e6d98f82
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-15 10:46:56 -0300

    Receba um número e diga se é positivo, negativo ou zero.

---
+++ exercicio009.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+let numero = parseInt(prompt('Digite um número: '))
+
+if (numero > 0){
+    console.log(`O número ${numero} é positivo.`)
+} else if (numero < 0){
+    console.log(`O número ${numero} é negativo.`)
+}else{
+    console.log('O número é zero.')
+}
