434418a5fa3895d8a04ac2fe7bc743db0b1ae6c9
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-15 10:39:20 -0300

    Use operadores de comparação e compare 3 numeros. Depois imprima.

---
+++ exercicio007.js
@@ -0,0 +1,10 @@
+let numero1 = '10'
+let numero2 = 12
+let numero3 = 10
+let comparacao1, comparacao2, comparacao3
+
+comparacao1 = numero1 === numero3
+comparacao2 = numero3 < numero2
+comparacao3 = numero2 != numero3
+
+console.log(comparacao1, comparacao2, comparacao3)
